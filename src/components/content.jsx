0ddb47fc4cc029a2a04daeb305ab58133d8905c6
dotun123 <justiceagunkejoye@gmail.com>
2023-05-17 21:28:44 +0100

    final commit

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -3,19 +3,27 @@ import "./content.css"
 import Input from './input';
 import {BsFillFileArrowDownFill,BsFillFileArrowUpFill} from "react-icons/bs"
 import {FiSettings} from "react-icons/fi"
+import{RxDot} from "react-icons/rx"
 
 const Content = () => {
  const[arrow,setArrow]=useState(false)
+
+
     return (
     <div className="mila__content ">
     <div className='card'>
+    <p><RxDot/>Swap</p>
+    <p> <RxDot/>Buy</p>
+    
     <div className='settings'>
-    <FiSettings color='black' size={20}/>
+   
+    <FiSettings  size={20}/>
+   
     </div>
    <Input/>
    <div  className="mila__arrow">
    {arrow
-          ? <BsFillFileArrowDownFill color="var(--color--content)" size={20} onClick={() => setArrow(false)} />
+          ? <BsFillFileArrowDownFill color="black" size={20} onClick={() => setArrow(false)} />
           : <BsFillFileArrowUpFill color="var(--color-content)" size={20} onClick={() => setArrow(true)} />}
    </div>
    <Input/>
