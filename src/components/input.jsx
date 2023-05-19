9806f474ed0694ef2a677b2707588a9e369e6c46
dotun123 <justiceagunkejoye@gmail.com>
2023-05-19 12:17:58 +0100

    test

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./input.css"
+import{IoIosArrowDown} from "react-icons/io"
 
 
 function Input() {
   return (
     <div className="mila__input">
-      <input placeholder='0'/>
+      <input placeholder='0' readonly="readonly"/>
+      <button>Select token<IoIosArrowDown/></button>
     </div>
   )
 }
